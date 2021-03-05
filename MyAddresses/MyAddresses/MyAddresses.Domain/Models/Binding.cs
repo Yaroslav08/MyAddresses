ddfaa726dd17e41c6324317c31b18396779b44ae
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-05 23:51:28 +0200

    Added Binding model

---
+++ MyAddresses/MyAddresses/MyAddresses.Domain/Models/Binding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MyAddresses.Domain.Models
+{
+    public class Binding : BaseModel<Guid>
+    {
+        [Required]
+        public bool IsPrivate { get; set; } = false;
+        [MinLength(1), MaxLength(200)]
+        public string HouseName { get; set; }
+        [MinLength(1), MaxLength(200), Url]
+        public string Site { get; set; } 
+    }
+}
