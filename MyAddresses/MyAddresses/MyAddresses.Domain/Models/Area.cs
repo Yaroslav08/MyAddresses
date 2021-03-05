9c03d182fd45ffcd08f3ba832ca58c07accf6adf
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-05 22:07:17 +0200

    Added Area model

---
+++ MyAddresses/MyAddresses/MyAddresses.Domain/Models/Area.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MyAddresses.Domain.Models
+{
+    public class Area : BaseModel<Guid>
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
