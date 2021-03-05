e52f3acb34577ad2c100a0d44d174d4b28972423
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-05 21:25:20 +0200

    Added BaseModel

---
+++ MyAddresses/MyAddresses/MyAddresses.Domain/Models/BaseModel.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAddresses.Domain.Models
+{
+    public class BaseModel<TypeId>
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public TypeId Id { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        [MinLength(1), MaxLength(50)]
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        [MinLength(1), MaxLength(50)]
+        public string UpdatedBy { get; set; }
+    }
+}
--- MyAddresses/MyAddresses/MyAddresses.Domain/Models/Country.cs
+++ MyAddresses/MyAddresses/MyAddresses.Domain/Models/Country.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 namespace MyAddresses.Domain.Models
 {
-    public class Country
+    public class Country : BaseModel<int>
     {
-
+        [Required, MinLength(2), MaxLength(100)]
+        public string Name { get; set; }
+        [MinLength(5), MaxLength(200)]
+        public string Population { get; set; }
+        [StringLength(5000)]
+        public string History { get; set; }
     }
 }
