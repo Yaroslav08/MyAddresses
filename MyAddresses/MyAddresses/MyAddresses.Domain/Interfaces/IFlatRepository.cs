031f0254cab3f20b4c47dd0c487b5be7fc5e2c7b
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-06 15:09:46 +0200

    Added interfaces to entities

---
+++ MyAddresses/MyAddresses/MyAddresses.Domain/Interfaces/IFlatRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyAddresses.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MyAddresses.Domain.Interfaces
+{
+    public interface IFlatRepository : IRepository<Flat>
+    {
+
+    }
+}
