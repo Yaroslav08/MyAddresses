bb294cbf17ddd57f238c6087c25e2e1ba26bbab5
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-06 14:51:25 +0200

    Added models to Context

--- MyAddresses/MyAddresses/MyAddresses.Infrastructure.Data/Context/MyAddressContext.cs
+++ MyAddresses/MyAddresses/MyAddresses.Infrastructure.Data/Context/MyAddressContext.cs
@@ -11,6 +11,13 @@ namespace MyAddresses.Infrastructure.Data.Context
     {
         private const string LocalConn = "Server=(localdb)\\MSSQLLocalDB;Database=MyAddressesDb;Trusted_Connection=True;";
 
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<Settlement> Settlements { get; set; }
+        public DbSet<Street> Streets { get; set; }
+        public DbSet<Binding> Bindings { get; set; }
+        public DbSet<Flat> Flats { get; set; }
         public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
